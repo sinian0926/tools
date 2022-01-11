bde5477b343e430c64b6d857761809941a428669
sssrrr879@126.com <A>
2022-01-11 23:45:02 +0800

    自动更新SVN：不更新文件中以井号#开头的行

--- auto/AutoUpSVN.py
+++ auto/AutoUpSVN.py
@@ -8,6 +8,11 @@ def auto_update_svn(txt_path):
         for line in f.readlines():
             # print(line)
             pro_line = line.strip()
+
+            # 过滤以井号(#)开头的
+            if '#'.__eq__(pro_line[0]):
+                continue
+
             # print(pro line)
             print('更新-', pro_line)
             os.chdir(pro_line)
@@ -21,7 +26,7 @@ def auto_update_svn(txt_path):
 
 if __name__ == '__main__':
     # paths=[r"D:\5VN-Documents\结算业务组”，r"D:\SVN-Documents\公共内容”，r"D:\5VN-Code\SCS\LXZZ"r"D:\SVWr"D:\SVW-Code\STS\LXZZ"]
-    PROJECT_NAME = r'base_tools'
+    PROJECT_NAME = r'tools'
     root_path = get_root_path(project_name=PROJECT_NAME)
     # 要更新的目录存放在下面这个文档中
     source_path = r'\exts\autoUpSVN.txt'
